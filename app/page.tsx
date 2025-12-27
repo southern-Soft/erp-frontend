2acf785f4445648d83a9ea536fb8c14e64295f0a
Sama-E-Shan <sama.shoummo@gmail.com>
2025-12-27 10:27:57 +0600

    page modified

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">Welcome to Southern Apparels ERP</h1>
+    </div>
+  );
+}
